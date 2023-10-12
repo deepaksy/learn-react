91f7d27199964d8d3c9d988224f6df80dc80fa84
deepaksy <deepaksy28@gmail.com>
2023-10-12 23:51:37 +0530

    Update theme storage logic

--- src/services/useTheme.jsx
+++ src/services/useTheme.jsx
@@ -1,19 +1,43 @@
-import { useLayoutEffect, useMemo, useState } from 'react'
+import { useLayoutEffect,  useState } from 'react'
 
 const useTheme = () => {
     const [isDark,setIsDark] = useState(false)
+    
     useLayoutEffect(()=>{
-        if(isDark){
-            document.documentElement.setAttribute('theme','dark')
+        console.log(`Initial update functions uselayouteffect called.`);
+        if(localStorage.getItem('theme')===null){
+            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                setIsDark(true)
+                storeThemePreference('dark')
+            }
+            else{
+                setIsDark(false)
+                storeThemePreference('light')
+            }
+            console.log(`theme undefined`);
         }
         else{
-            document.documentElement.setAttribute('theme','light')
+            setIsDark(localStorage.getItem('theme')==='dark'?true:false)
         }
+    },[])
+
+    useLayoutEffect(()=>{
+        const getThemeValue = isDark?'dark':'light'
+        if(isDark){
+            document.documentElement.setAttribute('theme',getThemeValue)
+        }
+        else{
+            document.documentElement.removeAttribute('theme')
+        }
+        storeThemePreference(getThemeValue)
     },[isDark])
 
-    useMemo(()=>{
-        
-    },[])
+    
+
+    const storeThemePreference = (isdark) =>{
+        localStorage.setItem('theme',isdark)
+    }
+
     const toggleTheme = () =>{
         setIsDark(!isDark)
     }
