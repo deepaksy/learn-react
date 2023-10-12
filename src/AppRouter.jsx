e3633e7b79abf7e8d36f14663240467a11a1ff7c
deepaksy <deepaksy28@gmail.com>
2023-10-12 15:48:54 +0530

    update AppRouter.jsx

---
+++ src/AppRouter.jsx
@@ -0,0 +1,31 @@
+import {
+BrowserRouter as Router,
+Routes,
+Route
+} from 'react-router-dom'
+import App from './App'
+import Navbar from './components/Navbar'
+import Components from './Pages/Components'
+import JSX from './Pages/JSX'
+import Props from './Pages/Props'
+import Memo from './Pages/Memos'
+import Refs from './Pages/Refs'
+import Portals from './Pages/Portals'
+const AppRouter = () =>{
+    return(
+        <Router>
+        <Navbar/>
+        <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/components' element={<Components/>} />
+        <Route path='/jsx' element={<JSX/>} />
+        <Route path='/props' element={<Props/>} />
+        <Route path='/memo' element={<Memo/>}/>
+        <Route path='/refs' element={<Refs/>}/>
+        <Route path='/portals' element={<Portals/>}/>
+        </Routes>
+        </Router>
+    )
+}
+
+export default AppRouter
