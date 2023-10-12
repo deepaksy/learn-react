e438eab5105444435bb7149c53d2cc08ee84f1c1
deepaksy <deepaksy28@gmail.com>
2023-10-12 15:53:16 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Introduction from './Pages/Introduction';
 // import Button from './components/Button';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <Navbar/>
     <Introduction/>
     {/* <Button text={'hello'}/> */}
     <div style={{minHeight:'100vh'}}>
