c87cf45823255e5a8f118460f6cc29a7d552fd03
deepaksy <deepaksy28@gmail.com>
2023-10-13 16:04:51 +0530

    update AppRouter.jsx

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -11,6 +11,10 @@ import Props from './Pages/Props'
 import Memo from './Pages/Memos'
 import Refs from './Pages/Refs'
 import Portals from './Pages/Portals'
+import HOC from './Pages/HOC'
+import ErrorBoundary from './Pages/ErrorBoundary'
+import Renderprops from './Pages/RenderProps'
+import Context from './Pages/Context'
 const AppRouter = () =>{
     return(
         <Router>
@@ -23,6 +27,10 @@ const AppRouter = () =>{
         <Route path='/memo' element={<Memo/>}/>
         <Route path='/refs' element={<Refs/>}/>
         <Route path='/portals' element={<Portals/>}/>
+        <Route path='/hoc' element={<HOC/>}/>
+        <Route path='/error-boundary' element={<ErrorBoundary/>}/>
+        <Route path='/render-props' element={<Renderprops/>}/>
+        <Route path='/context' element={<Context/>}/>
         </Routes>
         </Router>
     )
